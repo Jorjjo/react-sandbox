7342f92e890c22632b6f8879c72ea583168e68c7
Daria Mochalova <shichimiya74@gmail.com>
2026-03-12 17:03:28 +0500

    oops

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,72 +1,8 @@
 type RatingProps = {
-    value: 0 | 1 | 2 | 3 | 4 | 5
-}
+    value: 0 | 1 | 2 | 3 | 4 | 5;
+};
 
 export function Rating(props: RatingProps) {
-    // switch (props.value) {
-    //     case 1:
-    //         return (
-    //             <div>
-    //                 <Star selected={true} />
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //             </div>
-    //         );
-    //     case 2:
-    //         return (
-    //             <div>
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //             </div>
-    //         );
-    //     case 3:
-    //         return (
-    //             <div>
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //             </div>
-    //         );
-    //     case 4:
-    //         return (
-    //             <div>
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //                 <Star selected={false} />
-    //             </div>
-    //         );
-    //     case 5:
-    //         return (
-    //             <div>
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //                 <Star selected={true} />
-    //             </div>
-    //         );
-
-    //     default:
-    //         return (
-    //             <div>
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //                 <Star selected={false} />
-    //             </div>
-    //         );
-    // }
-
     return (
         <div>
             <Star selected={props.value >= 1} />
@@ -79,8 +15,8 @@ export function Rating(props: RatingProps) {
 }
 
 type StarProps = {
-    selected: boolean
-}
+    selected: boolean;
+};
 
 function Star(props: StarProps) {
     if (props.selected) {
